1008240d0c5f6f73ed8016aec1d4e0b657f16fdc
Kamil Podwika <kamilpodwika@gmail.com>
2023-12-01 19:57:23 +0100

    navbar

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -4,7 +4,31 @@ import { Outlet } from "react-router"
 export default function Layout() {
     return(
         <div>
-            <h1>Header</h1>
+            <div class="nav">
+                <input type="checkbox" id="nav-check"/>
+                <div class="nav-header">
+                    <div class="nav-title">
+                    Kamil Podwika
+                    </div>
+                </div>
+                <div class="nav-btn">
+                    <label for="nav-check">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    </label>
+                </div>
+                
+                <div class="nav-links">
+                    <a href="/Events" target="_blank">Events</a>
+                    <a href="/Categories" target="_blank">Categories</a>
+                    <a href="/Table" target="_blank">Table</a>
+                    {/* <a href="https://codepen.io/jo_Geek/" target="_blank">Codepen</a>
+                    <a href="https://jsfiddle.net/user/jo_Geek/" target="_blank">JsFiddle</a> */}
+                </div>
+  
+        </div>
+
             <Outlet/>
             <h3>Footer</h3>
         </div>
