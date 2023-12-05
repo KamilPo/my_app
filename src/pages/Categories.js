8c8d9ec0ad4dc07d3a247cb1955535b09897c6b1
Kamil Podwika <kamilpodwika@gmail.com>
2023-12-05 22:38:24 +0100

    some changes

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -3,21 +3,32 @@ import categoriesData from "../data/categoriesData.js";
 import { Link } from "react-router-dom";
 
 export default function Categories(props) {
+
+  const categoryCards = props.categories.map((category, index) => (
+    // <article key={index} className={"categories--card"}>
+    //     <Link className="link" to={`/Categories/${index}`}>
+    //     <div className="card--name">{category.name}</div>
+    //     </Link>
+
+    //     <span className="category--color" style={{backgroundColor: category.color}}></span>
+
+    // </article>
+
+    <div class="col-md-4 mb-4" key={index}>
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">{category.name}</h5>
+          <p class="card-text">{ "color: " +  category.color }</p>
+        </div>
+      </div>
+    </div>
+  ))
+
   return (
-    <>
-        <h1 className="categories--label">Categories</h1>
-            <div className="categories--container">
-                {props.categories.map((category, index) => (
-                <article key={index} className={"categories--card"}>
-                    <Link className="link" to={`/Categories/${index}`}>
-                    <div className="card--name">{category.name}</div>
-                    </Link>
-                    
-                    <span className="category--color" style={{backgroundColor: category.color}}></span>
-                               
-                </article>
-                ))}
-            </div>
-    </>
+    <div class="container mt-5">
+      <div class="row">
+        {categoryCards}
+      </div>
+    </div>
   );
 }
--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,40 +1,21 @@
 import React from "react"
 import { Outlet } from "react-router"
 import { Link } from "react-router-dom";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
 
 export default function Layout() {
-    return(
-        <div>
-            <div class="nav">
-                <input type="checkbox" id="nav-check"/>
-                <div class="nav-header">
-                    <div class="nav-title">
-                    Kamil Podwika
-                    </div>
+    return (
+        <>
+            <body style={{ backgroundColor: "rgb(230,230,230)" }}>
+                <Header />
+                <div class="container mt-5">
+                    <Outlet />
                 </div>
-                <div class="nav-btn">
-                    <label for="nav-check">
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                    </label>
-                </div>
-                
-                <div class="nav-links">
-                    <Link to={"/createEvent"}>Create Event</Link>
-                    <Link to={"/events"}>Events</Link>
-                    <a href="/categories">Categories</a>
-                    <a href="/createCategory">Create Category</a>
-                    <a href="/table">Table</a>
-                    {/* <a href="https://codepen.io/jo_Geek/" target="_blank">Codepen</a>
-                    <a href="https://jsfiddle.net/user/jo_Geek/" target="_blank">JsFiddle</a> */}
-                </div>
-  
-            </div>
-
-            <Outlet/>
-            <h3>Footer</h3>
-        </div>
-        
+                <br /><br /><br /><br /><br />
+                <Footer />
+            </body>
+        </>
     )
 }
