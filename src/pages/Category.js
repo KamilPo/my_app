f4874f333997257235d016fc01fb2a1b9a29fa27
Kamil Podwika <kamilpodwika@gmail.com>
2023-12-06 17:09:23 +0100

    Category alert changes

--- src/pages/Category.js
+++ src/pages/Category.js
@@ -1,19 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams, useNavigate, Link } from "react-router-dom";
 
 export default function Category(props) {
   const { id  } = useParams();
   const category = props.categories.find((category) => category.id == id );
+  const [validationError, setValidationError] = useState("");
   const navigate = useNavigate();
 
   if (!category) {
     return <div>Nie znaleziono wydarzenia</div>;
   }
 
+  const isCategoryUsed = () => {
+    // Sprawdź, czy kategoria jest używana w wydarzeniach
+    return props.events.some((event) => event.categoryId === category.id);
+  };
+
   const handleDelete = () => {
-    const updatedCategories = props.categories.filter((e) => e.id !== parseInt(id, 10));
-    props.setCategories(updatedCategories);
-    navigate('/categories');
+    if (isCategoryUsed()) {
+      setValidationError("Nie można usunąć kategorii, która jest używana w wydarzeniach.");
+    } else {
+      const updatedCategories = props.categories.filter((e) => e.id !== parseInt(id, 10));
+      props.setCategories(updatedCategories);
+      navigate("/categories");
+    }
   };
 
   return (
@@ -21,6 +31,11 @@ export default function Category(props) {
       <Link to="/categories" className="btn btn-primary mb-3">
         Back
       </Link>
+      {validationError && (
+        <div className="alert alert-danger" role="alert">
+          {validationError}
+          </div>
+      )}
       <div className="card">
         <div className="card-body">
           <h1 className="card-title">{category.name}</h1>
