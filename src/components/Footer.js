12b254e94b42195537dee2ed991be3e42901dd9b
Kamil Podwika <kamilpodwika@gmail.com>
2023-12-07 14:26:23 +0100

    footer changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function Footer() {
   return (
     <div class="footer">
-      <p>&copy; 2023 Kamil Podwika Yahoo</p>
+      <p>&copy; 2023 Kamil Podwika</p>
     </div>
   )
 }
